c472eb492c01d53539a14c1b51710cf15f79ba88
Viviana <400200339@esmad.ipp.pt>
2022-01-08 00:55:39 +0000

    +fontes adicionadas, mainpage 90% done visualmente (não responsivo)

--- src/views/Mainpage.vue
+++ src/views/Mainpage.vue
@@ -1,15 +1,177 @@
 <template>
   <div class="mainpage">
-
-	<div id="yellowContent">
-		<div>
-			<p class="mr-5">O autismo é parte deste mundo</p>
-			<p class="ml-5">e não de um mundo à parte!</p>
+	
+	<div id="yellowSection">
+		<!-- <div>
+			<img src="../assets/images/curvesBackground/mainpageYellow.png" width="100%">
 		</div>
 
-		<img src="../assets/images/mundo.png" width="20%">
+		<div id="yellowContent">
+			<div>
+				<p class="mr-5">O autismo é parte deste mundo</p>
+				<p class="ml-5">e não de um mundo à parte!</p>
+			</div>
+
+			<img src="../assets/images/mundo.png" width="20%">
+		</div> -->
+		<div id="yellowSection">
+			<table><tr><td></td><td rowspan=2>
+
+					<img src="../assets/images/curvesBackground/mainpageYellow.png" width="100%">
+
+			</td></tr><tr>
+			<td colspan=2>
+
+					<div id="yellowContent">
+						<div>
+							<p class="mr-5" style="color: black">O autismo é parte deste mundo</p>
+							<p class="ml-5" style="color: black">e não de um mundo à parte!</p>
+						</div>
+
+						<img src="../assets/images/mundo.png" width="20%">
+					</div>
+
+			</td></tr></table>
+		</div>
+
+		<div id="pinkSection">
+			<table style="width: 100%;"><tr><td></td><td rowspan=2>
+
+					<img src="../assets/images/curvesBackground/mainpagePink.png" width="50%">
+				
+			</td></tr><tr>
+			<td colspan=2>
+		
+					<div id="pinkContent" class="row">
+						<div class="col ml-5"></div>
+
+						<div class="col ml-5" id="divWelcomingText">
+							<h1>Bem-vindo ao <p style="color: #dd65b0; display: inline;">Emoti</p>!</h1>
+							<p style="width: 90%">O Emoti dedica-se ao apoio das crianças diagnosticadas com PEA, promovendo o reconhecimento das emoções através de imagens, assim como ajuda os familiares a compreender melhor o autismo.</p>
+						</div>
+					</div>
+
+			</td></tr></table>
+		</div>
+
+		<div id="blueSection">
+			<table style="width: 100%;"><tr><td></td><td rowspan=2>
+
+					<!-- <img src="../assets/images/curvesBackground/mainpageBlue.png" width="100%"> -->
+					<div class="d-flex justify-content-center">
+						<img src="../assets/images/curvesBackground/mainpageBlueDetails.png" width="90%" id="blueDetails">
+					</div>
+				
+			</td></tr><tr>
+			<td colspan=2>
+
+					<div id="blueContent" class="row">
+						<div class="col">
+							IMAGEMMMMMMMMMM HERE
+						</div>
+
+						<div class="col">
+							<div class="mt-5">
+								<h1>"Conheces esta expressão?"</h1>
+								<p style="width: 70%">Neste jogo tens de adivinhar a emoção associada a cada imagem que será mostrada. Joga todos os dias e torna-te um mestre das emoções!</p>
+								<button class="btn mt-5" id="btnPlay"><div class="my-2">JOGAR</div></button>
+							</div>
+							
+						</div>
+
+					</div>
+
+			</td></tr></table>
+		</div>
+
+		<div id="greenSection" class="mt-5">
+			<div id="greenHeader">
+				<h1>Navegue pelo <p style="color: #28A167; display: inline;">site</p>!</h1>
+				<div>
+					<img src="../assets/images/curvesBackground/mainpageGreenCurve.png" width="100%">
+				</div>
+			</div>
+
+			<div id="greenContent" class="row">
+				<div class="col ml-5 mt-5">
+
+					<p id="title">Jogos</p>
+					<p>Venha conhecer os jogos direcionados para crianças autistas</p>
+				</div>
+				<div class="col mt-5">
+					
+					<p id="title">Suporte familiar</p>
+					<p style="width: 90%;">O apoio familiar é crucial no desenvolvimento infantil, tire dúvidas e saiba como ter influência positiva na vida das crianças com PEA</p>
+				</div>
+				<div class="col mt-5">
+					
+					<p id="title">Psicólogos</p>
+					<p>Ao contar com um profissional já é meio caminho andado. Conheça os psicólogos disponíveis</p>
+				</div>
+				<div class="col mr-5 mt-5">
+					
+					<p id="title">Sobre nós</p>
+					<p>Descubra quem é a equipa EMOTI!</p>
+				</div>
+			</div>
+		</div>
+
 	</div>
 
+
+	<!-- RODAPÉ -->
+	<div id="footer">
+		<table style="width: 100%;"><tr><td></td><td rowspan=2>
+
+					<!-- <img src="../assets/images/curvesBackground/mainpageBlue.png" width="100%"> -->
+					<div class="d-flex justify-content-center">
+						<img src="../assets/images/curvesBackground/footerBG.png" width="100%" id="blueDetails">
+					</div>
+				
+			</td></tr><tr>
+			<td colspan=2>
+
+					<div class="row">
+						<div class="col mb-5" id="footerBtns">
+							<button class="btn mr-3" id="btnFooterLogin"><div class="my-1 mx-2">LOGIN</div></button>
+							<button class="btn mr-3" id="btnFooterRegister"><div class="my-1 mx-2">REGISTER</div></button>
+						</div>
+						<div class="col"></div>
+
+					</div>
+					<div class="row mt-5 m-5" id="footerLinks">
+						<div class="col">
+							<p id="linkTitle" style="display: inline;">EQUIPA </p>
+							<p style="display: inline;font-family: Pacifico; font-size: 15px;"> EMOTI</p>
+							<router-link to="/about">
+								<p class="mt-3">﹥Sobre nós</p>
+							</router-link>
+						</div>
+						<div class="col">
+							<p id="linkTitle">TUTORES</p>
+							<router-link to="/about">
+								<p>﹥FAQ</p>
+							</router-link>
+							<router-link to="/about">
+								<p>﹥Suporte Familiar</p>
+							</router-link>
+						</div>
+						<div class="col">
+							<p id="linkTitle">CRIANÇAS</p>
+							<router-link to="/about">
+								<p>﹥Jogos</p>
+							</router-link>
+						</div>
+						<div class="col">
+							<p id="linkTitle">PROFISSIONAIS</p>
+							<router-link to="/about">
+								<p>﹥FAQ</p>
+							</router-link>
+						</div>
+					</div>
+
+			</td></tr></table>
+	</div>
 	
   </div>
 </template>
@@ -26,12 +188,107 @@ export default {
 </script>
 
 <style>
-  #yellowContent {
-	background-image: url('../assets/images/curvesBackground/mainpageYellow.png');
-	background-size: cover;
-	display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-around;
-  }
+	h1 {
+		font-family: Baloo_2 extrabold;
+	} h1, p {
+		color: #606060;
+	}
+
+	#yellowContent {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-around;
+	}
+
+	#divWelcomingText {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+	}
+
+	#blueSection {
+		background-image: url('../assets/images/curvesBackground/mainpageBlue.png');
+		background-size: cover;
+	}
+
+	#blueContent div div, #blueContent div p {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	#btnPlay {
+		font-family: Baloo_2 extrabold;
+		background-color: #29ABE2;
+		color: #FFFFFF;
+		width: 15%;
+
+		position: relative;
+		right: 27%;
+
+		filter: drop-shadow(5px 4px 2px #00000030);
+	} #btnPlay:hover {
+		background-color: #FFFFFF;
+		color: #29ABE2;
+	}
+
+	#greenSection {
+		position: relative;
+		top: 5vh;
+	} #greenHeader {
+		display: flex;
+		justify-content: space-evenly;
+	} #greenContent {
+		background-color: #ADEACD;
+		position: relative;
+		bottom: 15px;
+		z-index: -1;
+	} #greenContent .col {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	} #greenContent .col p {
+		text-align: center;
+	} #title {
+		color: #28A167;
+		font-size: 25px;
+	}
+
+	#footer {
+		background-color: #ADEACD;
+	}
+
+	#footerBtns {
+		display: flex;
+		justify-content: center;
+	}
+
+	#btnFooterLogin {
+		font-family: Baloo_2 extrabold;
+		background-color: #81EFBA;
+		color: #FFFFFF;
+	} #btnFooterLogin:hover {
+		background-color: #FFFFFF;
+		color: #81EFBA;
+	}
+
+	#btnFooterRegister {
+		font-family: Baloo_2 extrabold;
+		background-color: #FFD167;
+		color: #FFFFFF;
+	} #btnFooterRegister:hover {
+		background-color: #FFFFFF;
+		color: #FFD167;
+	}
+
+	#footerLinks {
+		position: relative;
+		top: 10vh;
+	} #footerLinks .col p {
+		color: #FFFFFF;
+	} #linkTitle {
+		font-family: Baloo_2 bold;
+		font-size: 20px;
+	}
 </style>
