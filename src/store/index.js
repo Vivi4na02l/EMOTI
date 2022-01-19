6cc32db9b6db339bb155d872c953f62e044a15f2
Viviana <400200339@esmad.ipp.pt>
2022-01-19 14:10:28 +0000

    +base jogos

--- src/store/index.js
+++ src/store/index.js
@@ -13,8 +13,91 @@ export default new Vuex.Store({
           type: "admin"
         },
       ],
+
+    jogoRecognizeEmotion: [
+      {
+        name: "alegria",
+        images: [
+          {
+            addedBy: "admin",
+            image: "../assets/images/jogo/emojis/alegria.svg",
+          }
+        ]
+      },
+      {
+        name: "amor",
+        images: [
+          {
+            addedBy: "admin",
+            image: "../assets/images/jogo/emojis/amor.svg",
+          }
+        ]
+      },
+      {
+        name: "cansaço",
+        images: [
+          {
+            addedBy: "admin",
+            image: "../assets/images/jogo/emojis/cansaço.svg",
+          }
+        ]
+      },
+      {
+        name: "enojado",
+        images: [
+          {
+            addedBy: "admin",
+            image: "../assets/images/jogo/emojis/enojado.svg",
+          }
+        ]
+      },
+      {
+        name: "espanto",
+        images: [
+          {
+            addedBy: "admin",
+            image: "../assets/images/jogo/emojis/espanto.svg",
+          }
+        ]
+      },
+      {
+        name: "medo",
+        images: [
+          {
+            addedBy: "admin",
+            image: "../assets/images/jogo/emojis/medo.svg",
+          }
+        ]
+      },
+      {
+        name: "tristeza",
+        images: [
+          {
+            addedBy: "admin",
+            image: "../assets/images/jogo/emojis/tristeza.svg",
+          }
+        ]
+      },
+      {
+        name: "raiva",
+        images: [
+          {
+            addedBy: "admin",
+            image: "../assets/images/jogo/emojis/raiva.svg",
+          }
+        ]
+      }
+    ]
   },
   getters: {
+    getjogoRecognizeEmotion: (state) => { return state.jogoRecognizeEmotion },
+
+    getRandomImageEmotion: (state) => (emotion, image) => 
+    { return {emotion: state.jogoRecognizeEmotion[emotion].name, image: state.jogoRecognizeEmotion[emotion].images[image].image } },
+    
+    isEmotion: (state) => (username, password) => 
+    state.users.some(user => user.username == username && user.password == password),
+
   },
   mutations: {
   },
