f7953ae151126f3f25a5e7742c17f4b3997c1cef
Viviana <400200339@esmad.ipp.pt>
2022-01-27 10:31:21 +0000

    +sobreNos

--- src/views/SobreNos.vue
+++ src/views/SobreNos.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <form>
-			<label>Username</label>
-			<br />
-			<input type="text" required />
-			<br />
-			<br />
-			<label>Password</label>
-			<br />
-			<input type="password" required />
-			<br />
-			<br />
-			<input type="submit" value="Login" />
-		</form>
-  </div>
+    <div>
+        <div>
+            <img src="../assets/images/curvesBackground/Group 29.png" width="100%">
+        </div>
+        <div>
+            <h1 style="color: #606060">Sobre a equipa <span style="color: #ffd167">EMOTI</span>!</h1>
+			<img src="../assets/images/sobreNos.png" style="width: 70%">
+        </div>
+        
+        <NavBar></NavBar>
+        <Footer></Footer>
+    </div>
 </template>
+
+<script>
+    import Footer from '../components/Footer.vue'
+    import NavBar from '../components/NavBar.vue'
+    export default {
+        components: {
+            NavBar,
+            Footer
+        },
+	}
+</script>
+
+<style>
+
+</style>
